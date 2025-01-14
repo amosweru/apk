75762476284aacf843335ed1e476eee2ebf6ab1e
amosweru <amoowa@yahoo.com>
2025-01-14 00:57:29 -0800

    Update Dependencies.kt

--- .github/workflows/buildSrc/Dependencies.kt
+++ .github/workflows/buildSrc/Dependencies.kt
@@ -1,1 +1,7 @@
-
+object Dependencies {
+    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+    const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
+    const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    const val material = "com.google.android.material:material:${Versions.material}"
+    // Add more dependencies as needed
+}
