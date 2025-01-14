31eca703506ed02004253ae401969a0c06577488
amosweru <amoowa@yahoo.com>
2025-01-14 02:16:27 -0800

    Update gradlew.bat

--- .github/workflows/gradlew.bat
+++ .github/workflows/gradlew.bat
@@ -1,14 +1,1 @@
-@echo off
-rem Gradle wrapper script for Windows
 
-set DIR=%~dp0
-cd /d "%DIR%"
-
-rem Check if gradle-wrapper.jar exists
-if not exist gradle-wrapper.jar (
-    echo Could not find gradle-wrapper.jar. Please run gradle wrapper to generate it.
-    exit /b 1
-)
-
-rem Run Gradle
-java -Dfile.encoding=UTF-8 -Xmx1024m -classpath "gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
